d84a4ea39a82dd374d0d21d3ef5389324bd30467
ltogt <omnesiaorg@gmail.com>
2021-06-20 10:56:58 +0200

    init with boolswitch

---
+++ bin/dart_playground.dart
@@ -0,0 +1,5 @@
+import 'experiments/bool_switch.dart';
+
+void main(List<String> arguments) {
+  BoolSwitch.run();
+}
---
+++ bin/experiments/bool_switch.dart
@@ -0,0 +1,86 @@
+/// Motivation:
+///
+/// In some cases one wants to definetly cover all combinations of multiple boolean expressions
+/// § In Flutter didUpdateWidget when a controller can be passed
+///   -- NewHas && OldHad
+///   -- NewHas && OldHadNot
+///   -- NewHasNot && OldHad
+///   -- NewHasNot && OldHadNot
+///
+/// ```
+/// bool newHasController = widget.controller != null;
+/// bool oldHasController = oldWidget.controller != null;
+/// BoolSwitch.two(
+///   exp1: newHasController,
+///   exp2: oldHasController,
+///   tt: swapWidgetController,
+///   tf: swapLocalForWidgetController,
+///   ft: createLocalController,
+///   ff: () {},
+/// );
+/// ```
+
+class BoolSwitch {
+  BoolSwitch._();
+
+  static void run() {
+    String result = BoolSwitch.two(
+      exp1: false,
+      exp2: true,
+      tt: () => "tt",
+      tf: () => "tf",
+      ft: () => "ft",
+      ff: () => "ff",
+    );
+  }
+
+  static T two<T>({
+    required bool exp1,
+    required bool exp2,
+    required T Function() tt,
+    required T Function() tf,
+    required T Function() ft,
+    required T Function() ff,
+  }) {
+    if (exp1) {
+      if (exp2) return tt();
+      return tf();
+    } else {
+      if (exp2) return ft();
+      return ff();
+    }
+  }
+
+  static T three<T>({
+    required bool exp1,
+    required bool exp2,
+    required bool exp3,
+    required T Function() ttt,
+    required T Function() ttf,
+    required T Function() tft,
+    required T Function() tff,
+    required T Function() ftt,
+    required T Function() ftf,
+    required T Function() fft,
+    required T Function() fff,
+  }) {
+    if (exp3) {
+      return BoolSwitch.two(
+        exp1: exp1,
+        exp2: exp2,
+        tt: ttt,
+        tf: tft,
+        ft: ftt,
+        ff: fft,
+      );
+    }
+    return BoolSwitch.two(
+      exp1: exp1,
+      exp2: exp2,
+      tt: ttf,
+      tf: tff,
+      ft: ftf,
+      ff: fff,
+    );
+  }
+}
